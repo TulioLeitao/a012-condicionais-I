7febc62fdd046dcbea949e5a60252d13f6bc1607
TulioLeitao <tulioleitao@gmail.com>
2022-08-04 17:58:04 -0300

    exercício a012

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,19 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 23;
+let ensinoMedio = true;
+let cursandoFaculdade = false;
 
-if(){
-
-}else{
-	
+if (idade >= 18){
+    console.log ('A pessoa é maior de idade')
+} else {
+    console.log ('A pessoa é menor de idade')
+};
+if (ensinoMedio){
+    console.log ('A pessoa terminou o ensino médio')
+} else {
+    console.log ('A pessoa não terminou o ensino médio')
+};
+if (cursandoFaculdade) {
+    console.log ('A pessoa está cursando faculdade')
+} else {
+    console.log ('A pessoa não está cursando a faculdade')
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt('Escreva aqui sua nacionalidade');
+
+if (nacionalidade === 'brasileira' ) {
+    alert ('sua nacionalidade é brasileira')
+} else if (nacionalidade === 'argentina') { 
+    alert ('sua nacionalidade é argentina')
+} else if (nacionalidade === 'uruguaia') { 
+    alert ('sua nacionalidade é uruguaia')
+} else if (nacionalidade === 'chilena') { 
+    alert ('sua nacionalidade é chilena')
+} else if (nacionalidade === 'colombiana') { 
+    alert ('sua nacionalidade é colombiana')
+} else {
+    alert ('nacionalidade não encontrada')
+};
